e8657265012fd3d7684085bd3f473811305f768a
Kowshika <e.s.kowshika2001@gmail.com>
2023-02-13 09:26:38 +0530

    all commited

--- js/login.js
+++ js/login.js
@@ -14,20 +14,42 @@ $(document).ready(function () {
             $("#usercheck").hide()
             $("#passcheck").html("Enter Password")
         }
+        // form_data = new FormData();
+        // form_data.append("username",$('#username').val());
+        // form_data.append('password',$('#password').val());
+        const data = {"username":$("#username").val(),"password":$("#password").val()}
+
+        
         $.ajax({
-            method : 'POST',
-            url : '../course_suite/main.py',
-            data : {"usernmae":$("#username").val(),"password":$("#password").val()},
-            datatype : 'JSON',
+            type : 'POST',
+            url : 'http://localhost:5000/login',
+            // headers: {
+            //     'Content-Type': 'multipart/form-data/application/json'
+            // },
+            //data : JSON.stringify(data),
+            data : $("#datas").serialize(),
+            // data : {
+            //     username:$('#username').val(),
+            //     password:$('#password').val()
+            // },
+            //datatype : 'JSON',
+            //data : data,
+            processData : false,
             success:function(data){
                 console.log(data);
-                var d=JSON.parse(data);
-                //console.log()
-                // if(d=="success"){
-                //     console.log("success");   
-                // }else{
-                //     console.log("incorrect");
-                // }
+                console.log(data['status']);
+                if(data['status']=="success" || data['status']=="exists"){
+                    window.location.href='course_id.html'
+                    console.log("success") 
+                    localStorage.setItem('key', data['key'])  
+                }else if(data['status']=="incorrect"){
+                    $("#error").html("Password incorrect")
+                }else if(data['status']=="invalid"){
+                    $("#error").html("Invalid Username")
+                }
+            },
+            error: function(response){
+                console.log(response)
             }
         }); 
     });
