ec5743810cd05e613a6bd3dd26f25adc511e094e
Emayakeerthi N <nemayakeerthi@gmail.com>
2023-02-15 23:29:31 +0530

    error: syntax

--- js/course_id.js
+++ js/course_id.js
@@ -50,82 +50,97 @@ $(document).ready(function () {
   $("#course_form").submit(function (e) {
     e.preventDefault();
     var course_id = document.getElementById("courseId").value;
-     if (course_id=="") {
+    if (course_id=="") {
       $("#error").html("<br>This Field Required");
-    } else {
-       var dict = {"course_id": $("#courseId").val()};
-      
+    }
+    else {
+      var dict = {"course_id": $("#courseId").val(), "session_id": session_id};
+
       $.ajax({
         method:'POST',
         url: 'http://127.0.0.1:5000/course_status',
         datatype: 'JSON',
         data: JSON.stringify(dict),
         contentType: "application/json",
-        success: function (data) {
-          if (data.status == "success") {
-            changes = data.changes;
+        success: function(data){
+          if(data.status == "success"){ 
+            $.when(
+              $.ajax({
+                method: "POST",
+                url: 'http://127.0.0.1:5000/course_changes',
+                datatype: 'JSON',
+                data: JSON.stringify(dict),
+                contentType: "application/json",
+                success: function(data){
+                  console.log("working-course_changes api");
+                }
+              }),
 
-            // document.getElementById("course_id_page").style.display = "none";
+              $.ajax({
+                method: "POST",
+                url: 'http://127.0.0.1:5000/check_video',
+                datatype: 'JSON',
+                data: JSON.stringify(dict),
+                contentType: "application/json",
+                success: function(data){
+                  if(data.status == true){
+                    document.getElementById("video_check").style.display = "block"
+                  }
+                  else{
+                    document.getElementById("video_close").style.display = "block";
+                    document.getElementById("video_check").style.display = "none";
+                  }
+                }
+              }),
+
+              $.ajax({
+                method: "POST",
+                url: 'http://127.0.0.1:5000/check_quiz',
+                datatype: 'JSON',
+                data: JSON.stringify(dict),
+                contentType: "application/json",
+                success: function(data){
+                  if(data.status == true){
+                    document.getElementById("quiz_check").style.display = "block"
+                  }
+                  else{
+                    document.getElementById("quiz_close").style.display = "block";
+                    document.getElementById("quiz_check").style.display = "none";
+                  }
+                }
+              })
+            ).then(()=>{
+              $.ajax({
+                type: "POST",
+                url: 'http://127.0.0.1:5000/check_live',
+                datatype: 'JSON',
+                data: JSON.stringify(dict),
+                contentType: "application/json",
+                success: function(data){
+                  if(data.live == true){
+                    $("#livestatusstatus").html("Live");
+                    document.getElementById("livestatusstatus").style.color = "green";
+                    document.getElementById("live_check").style.display = "block";
+                  }else {
+                    $("#livestatusstatus").html("No Live");
+                    document.getElementById("livestatusstatus").style.color = "red";
+                    document.getElementById("live_check").style.display = "none";
+                    document.getElementById("live_close").style.display = "block";
+                  }
+                }
+            }, ()=>{});
+            document.getElementById("course_id_page").style.display = "none";
             document.querySelector(".content").style.display = "none";
             $("#coursenamestatus").html(data.course_name);
             $("#courseidstatus").html(data.course_id);
             $("#courselangstatus").html(data.lang);
-            if (data.live == true) {
-              $("#livestatusstatus").html("Live");
-              document.getElementById("livestatusstatus").style.color = "green";
-              document.getElementById("live_check").style.display = "block";
-            } else {
-              $("#livestatusstatus").html("No Live");
-              document.getElementById("livestatusstatus").style.color = "red";
-              document.getElementById("live_check").style.display = "none";
-              document.getElementById("live_close").style.display = "block";
-            }
-            if (data.update == true) {
-              $("#updatesstatus").html("Available");
-              document.getElementById("updatesstatus").style.color = "green";
-              const updateBtn = document.getElementById("update_btn");
-              updateBtn.disabled = false;
-              updates_det = {
-                "course_id" : course_id,
-                'update': true
-              };
-
-              //const availBtn = document.getElementById("updatesstatus");
-            }
-            else {
-              $("#updatesstatus").html("Unavailable");
-              document.getElementById("updatesstatus").style.color = "red";
-            }
-            if (data.video == true) {
-              document.getElementById("video_check").style.display = "block";
-            }
-            else {
-              document.getElementById("video_close").style.display = "block";
-              document.getElementById("video_check").style.display = "none";
-            }
-            if (data.quiz == true) {
-              document.getElementById("quiz_check").style.display = "block";
-            }
-            else {
-              document.getElementById("quiz_close").style.display = "block";
-              document.getElementById("quiz_check").style.display = "none";
-            }
-
-            // document.getElementById("course_id_individual_page").style.display = "block";
-            document.querySelector(".container").style.display = "block";
-            //alert("Changes :\n"+JSON.stringify(changes));
-            
           }
-          if(data.status =="login"){
-            window.location.href="login.html";
-          }
-          if(data.status == "invalid"){
-            $("#error").html("<br>Please enter valid course ID");
-          }
+        },
+        error: function(response){
+          console.log(response);
         }
           
       })
- 
     }
   });
 
--- js/logout.js
+++ js/logout.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
     $("#logout").on("click", function () { 
         $.ajax({
             type : 'POST',
-            url : 'http://localhost:5000/logout',     
+            url : 'http://localhost:5000/session_delete',
+            data: JSON.stringify({"session_id": window.localStorage.getItem("key")}),
+            dataType: "JSON",
+            contentType: "application/json",     
             success:function(data){
                 if(data.status == "success"){
                     localStorage.removeItem('key');
