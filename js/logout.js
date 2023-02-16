230e52086acbc75fda829599087947eb5e6da39f
Emayakeerthi N <nemayakeerthi@gmail.com>
2023-02-16 10:47:47 +0530

    logout fixed

--- js/logout.js
+++ js/logout.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     $("#logout").on("click", function () { 
         $.ajax({
             type : 'POST',
-            url : 'http://localhost:5000/logout',     
+            url : 'http://localhost:5000/session_delete',  
+            data:  JSON.stringify({"session_id":window.localStorage.getItem("key")}),
             success:function(data){
                 if(data.status == "success"){
                     localStorage.removeItem('key');
                 }
                 window.location.href="login.html"; 
+                window.localStorage.removeItem("time");
             }
         }); 
     });
